331efcb4c78ca938bf0199b760ca42264262ad29
huy209vn <duchuytran12341@gmail.com>
2025-06-22 15:40:17 +0700

    Add static assets

---
+++ stillness_grove/static/js/main.js
@@ -0,0 +1,8 @@
+import BreathingOverlay from './breathingOverlay.js';
+import Ripple from './ripple.js';
+
+// Initialize modules when DOM is ready
+window.addEventListener('DOMContentLoaded', () => {
+    BreathingOverlay.init();
+    Ripple.init();
+});
