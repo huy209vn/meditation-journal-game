96b7eda2648b445eba22773c8f5b00d66b09f4e1
huy209vn <duchuytran12341@gmail.com>
2025-06-22 15:54:00 +0700

    Add intro fade and buttons UI

--- stillness_grove/static/js/main.js
+++ stillness_grove/static/js/main.js
@@ -1,8 +1,10 @@
 import BreathingOverlay from './breathingOverlay.js';
 import Ripple from './ripple.js';
+import Intro from './intro.js';
 
 // Initialize modules when DOM is ready
 window.addEventListener('DOMContentLoaded', () => {
+    Intro.init();
     BreathingOverlay.init();
     Ripple.init();
 });
