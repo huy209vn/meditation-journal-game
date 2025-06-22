0fcb6a11a86cdf168d0546c351f6305973ac0099
huy209vn <duchuytran12341@gmail.com>
2025-06-22 15:39:57 +0700

    Add placeholder journal views and URL patterns

--- stillness_grove/journal/views.py
+++ stillness_grove/journal/views.py
@@ -1,1 +1,13 @@
-# Journal views placeholder
+"""Placeholder views for the journal app."""
+
+from django.http import HttpResponse
+
+
+def entry_list(request):
+    """Return a simple list of journal entries."""
+    return HttpResponse("Journal entry list placeholder")
+
+
+def entry_create(request):
+    """Return a placeholder page for creating a new journal entry."""
+    return HttpResponse("Journal entry creation placeholder")
